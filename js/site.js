338778d89cef426e25b487fa805933c7ca50f7f6
Mitansh Chaudhari <93405512+MitChaudhari@users.noreply.github.com>
2023-03-02 17:14:56 -0600

    finished faze 1 of styling

--- js/site.js
+++ js/site.js
@@ -3,16 +3,17 @@ const ageInput = document.querySelector('#age');
 
 form.addEventListener('submit', (event) => {
   event.preventDefault();
-  const age = ageInput.value;
-  if (age < 17) {
-    const error = document.querySelector('#error-message');
-    if (!error) {
-      const errorMessage = document.createElement('p');
-      errorMessage.id = 'error-message';
-      errorMessage.textContent = 'You must be 17 or older to sign up.';
-      form.appendChild(errorMessage);
-    }
-  } else {
-    form.submit();
+  const nameInput = document.getElementById('name');
+  const emailInput = document.getElementById('email');
+  const ageInput = document.getElementById('age');
+
+  if (ageInput.value < 17) {
+    alert('Sorry, you must be at least 17 years old to join the beta tester list.');
+    return;
   }
+ else{
+    form.submit();
+    alert("Great Job!! You have successfully joined the tester list.");
+    return;
+}
 });
